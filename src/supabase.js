ea06a1b61bd85b90ca2d12b01719ed833b0feb5a
AQand-AQand <70608078+AQand-AQand@users.noreply.github.com>
2026-06-13 20:47:43 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Mengambil URL dan KEY secara otomatis dari sistem environment variables cloud Anda
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
